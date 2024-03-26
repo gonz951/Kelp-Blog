6109f6761c1615a3cdabdfaeab43fede2bccbce6
Fel <felgonzalez951@gmail.com>
2024-03-26 13:04:01 -0600

    added id for post button, added consts for things needed in warning function

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,9 +1,13 @@
 const usernameInput = document.querySelector('#username');
 const titleInput = document.querySelector('#title');
 const contentInput = document.querySelector('#content');
+const postButton = document.querySelector('#post')
+const msgDiv = document.querySelector('#warning');
 
-renderLastPosted();
+// renderLastPosted();
 
 function displayMessage(type, message) {
-    
+    msgDiv.textContent = message;
+    msgDiv.setAttribute('class', type);
 }
+
