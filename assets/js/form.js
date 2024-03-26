31babd7c4baa2cd25c2912db0a536ea44b9532a9
Fel <felgonzalez951@gmail.com>
2024-03-26 00:07:14 -0600

    added some consts to start js for the form, added form tag around the inputs, added warning id for warning that will be added for lack of input later..

---
+++ assets/js/form.js
@@ -0,0 +1,9 @@
+const usernameInput = document.querySelector('#username');
+const titleInput = document.querySelector('#title');
+const contentInput = document.querySelector('#content');
+
+renderLastPosted();
+
+function displayMessage(type, message) {
+    
+}
