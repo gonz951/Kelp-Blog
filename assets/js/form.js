d2ea8f43d3c104a9e0474942656e869c1dc2e560
Fel <felgonzalez951@gmail.com>
2024-03-26 14:03:58 -0600

    added if else function for warning message, also added code for starting the local storage

--- assets/js/form.js
+++ assets/js/form.js
@@ -11,3 +11,27 @@ function displayMessage(type, message) {
     msgDiv.setAttribute('class', type);
 }
 
+postButton.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    const post = {
+        
+    }
+    const username = document.querySelector('#username').value;
+    const blogTitle = document.querySelector('#title').value;
+    const blogContent = document.querySelector('#content').value;
+
+    if (username === '') {
+        displayMessage('error', 'Please, add a username.');
+    } else if (blogTitle === '') {
+        displayMessage('error', 'Please, add a blog title.');
+    } else if (blogContent === '') {
+        displayMessage('error', 'Please, add blog content.')
+    } else {
+        displayMessage('success', 'Posted!');
+
+        
+    }
+
+})
+
